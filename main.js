f0e5b7ac0b867cca950e047ab2750eaeff6753e3
khaled refat <96064446+khaledrefat@users.noreply.github.com>
2022-11-25 21:25:18 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,160 @@
+// //varaibles
+// // var MyMame = "khaled refat";
+// // var myage=50;
+// -----------------------------------
+
+
+//     var numTotal = 100 ;
+//     if (numTotal==1000) {
+//     alert('ok right');  
+//     } else {
+// // طرق صباعة الناتج على الشاشه
+//             alert('no');  
+//             console .log('wrong');
+//             document.write('wwrong');
+
+//             document.getElementById('hh').innerHTML=(20%3);
+//     }
+
+// arithmtic operetor:
+    // + - * / ++ -- % 
+// comperison opertors:
+    //  <=  >=
+    // ==  بتقارن ع الفاليو
+    // ===   بتقارن ع الفاليو والداتا تايب
+    // !=    نوت ايكول   ف القيمه 
+    // !==   نوت ايكول ايكول   ف القيمه والداتا تايب
+    // +=
+
+// logical operetor
+        // && .... and
+        // || ....  or
+
+// ----------------------------------------------------------------
+function khaled(){
+    var div= document.getElementById("hh");
+    if (div.style.display==='none'){
+        div.style.display='block';
+    }else{
+        div.style.display='none'
+    }
+}
+
+
+
+
+
+
+
+
+
+
+// ----------------------------------------------------------------
+
+// var num1=10;
+//     num2=20;
+//     num3=10;
+//     num4=20;
+//     num5=20;
+//     num6=20;
+//     num7=10;
+//     num8=20;
+//     num9=20;
+//     var sum =num1+num2+num3+num4+num5+num6+num7+num8+num9
+//         alert(sum);
+
+// // function KhaledRefat()  {
+// //     console.log("hi khjaled refat");
+// //     alert("2*3");
+// // }
+// // KhaledRefat();
+// -----------------------------------------
+// // var num1=2000
+// // var num2=3000
+// // alert (num1+num2);
+// // console.log(num1+num2);
+// // document.write(num1+num2);
+// // document.getElementById("total").innerHTML = num1 + num2;
+
+
+// ------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function toggleswitch() {
+// var div= document.getElementById("MyDiv");
+// if(div.style.display==="none"){
+//     div.style.display="block";
+// }
+// else{
+//     div.style.display="none";
+// }
+// }
+
+// ------------------------------------
+
+
+// function toggleswitch() {
+//     var div= document.getElementById("MyDiv");
+//     if(div.style.color==="black"){
+//         div.style.color="red";
+//     }
+//     else{
+//         div.style.color="black";
+//     }
+//     }
+// ------------------------------------
+
+    
+        // ------------------------------------
+
+        // function toggleswitch() {
+        //     var div= document.getElementById("MyDiv");
+        //     if(div.style.fontSize==="30"){
+        //         div.style.fontSize="15";
+        //     }
+        //     else{
+        //         div.style.fontSize="30";
+        //     }
+        //     }
+            
+        // ------------------------------------
+
+        function khaledd() {
+            var BODY = document.body;
+            BODY.classList.toggle("lightmode");
+            }
+
+        // ------------------------------------
+
+// function opennav(){
+//     document.getElementById("mySidenav").style.width="130px";
+//     document.getElementById("main").style.marginLeft="130px";
+
+// }
+// function closeNav(){
+//     document.getElementById("mySidenav").style.width="0px";
+//     document.getElementById("main").style.marginLeft="0px";
+
+// }
+
+
+
+
+// // types
+// //condtions
+// //loop
+// //functions
+// //scope
+// //اجبارىعارفهم ف اي لغة برمجه
